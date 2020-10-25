29c0d8ad937df7d8caeb6e70ef04106828140f86
radzul12 <rafael.dzul.us@gmail.com>
2020-10-25 01:26:49 -0500

    Subiendo por primeravez el proyecto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App" >
+    <h1> Hola soy rafael </h1>
+    {/* Header */}
+    <Header/>
+  {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB8WZZ0O4kKjT2FKeGsJR2ZBKLkNO7JMa0",
+  authDomain: "clone-927f3.firebaseapp.com",
+  databaseURL: "https://clone-927f3.firebaseio.com",
+  projectId: "clone-927f3",
+  storageBucket: "clone-927f3.appspot.com",
+  messagingSenderId: "512242385660",
+  appId: "1:512242385660:web:a2807716be874e1f558d79",
+  measurementId: "G-VGSWLC3RFJ"
+};
