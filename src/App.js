eaed8ee369bf1c68747ae6536d2c0c1bcdd19fec
radzul12 <rafael.dzul.us@gmail.com>
2020-10-25 23:43:35 -0600

    agregnao app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 import Header from './Header';
-import Home from './Home';
 
 function App() {
   return (
     <div className="App" >
     {/* Header */}
-      <Header />
-      <Home/>
+    <Header/>
+  {/* Home */}
     </div>
   );
 }
 
 export default App;
-// Video minute 1:19 
+// Agregando Cambios
