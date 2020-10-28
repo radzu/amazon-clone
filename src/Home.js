8965702d101dfef6a1801b6a9d396775845c7c54
radzul12 <rafael.dzul.us@gmail.com>
2020-10-27 21:54:38 -0600

    productos

--- src/Home.js
+++ src/Home.js
@@ -14,6 +14,7 @@ function Home() {
         <div className="home__row">
           <Product/>
           <Product/>
+          <Product/>
         </div>
         <div className="home__row">
           {/* Product  */}
